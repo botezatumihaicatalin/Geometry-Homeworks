fe1f64765d65dafe684c50f1d681ba3382a34b85
botezatumihaicatalin <botezatu.mihaicatalin@gmail.com>
2014-11-30 12:21:35 +0200

    Added colours:

--- Pool/ball.h
+++ Pool/ball.h
@@ -12,11 +12,30 @@ enum CollisionState {
     Tangent
 };
 
+enum BallColor {
+    Red,
+    Blue,
+    Purple,
+    Green,
+    Black,
+    Yellow,
+    White,
+    Brown,
+    Orange
+};
+
+enum BallType {
+    Striped,
+    Filled
+};
+
 class Ball: public Circle {
 public:
 	Vector2D Direction;
-	Ball(const Vector2D & direction , const double & x = 0.0, const double & y = 0.0, const double & radius = 0.0) : Direction(direction) , Circle(x , y , radius) {}
-	Ball(const Vector2D & direction , const Point2D & center , const double & radius) : Direction(direction) , Circle(center , radius) {}
+	BallColor Color;
+	BallType Type;
+	Ball(BallColor color, BallType type, const Vector2D & direction , const double & x = 0.0, const double & y = 0.0, const double & radius = 0.0) : Color(color) , Type(type), Direction(direction) , Circle(x , y , radius) {}
+	Ball(BallColor color, BallType type, const Vector2D & direction , const Point2D & center , const double & radius) : Color(color), Type(type), Direction(direction) , Circle(center , radius) {}
 	CollisionState Collides(const Ball & ball) const;
 	CollisionState Collides(const Segment & segment) const;
 	void Collide(Ball & ball);
--- Pool/scene.cpp
+++ Pool/scene.cpp
@@ -51,33 +51,34 @@ vector<Circle> Scene::tablePockets = {
 	Circle(Constants::HoleDiameter / 2, Constants::HoleDiameter / 2, Constants::HoleDiameter / 2),
 	Circle(1200 / 2, Constants::HoleDiameter / 2, Constants::HoleDiameter / 2),
 	Circle(1200 - Constants::HoleDiameter / 2, Constants::HoleDiameter / 2, Constants::HoleDiameter / 2),
+	Circle(1200 - Constants::HoleDiameter / 2, 700 - Constants::HoleDiameter / 2, Constants::HoleDiameter / 2),
 	Circle(Constants::HoleDiameter / 2, 700 - Constants::HoleDiameter / 2, Constants::HoleDiameter / 2),
 	Circle(1200 / 2, 700 - Constants::HoleDiameter / 2, Constants::HoleDiameter / 2),
-	Circle(1200 - Constants::HoleDiameter / 2, 700 - Constants::HoleDiameter / 2, Constants::HoleDiameter / 2)
+
 };
 
 vector<Ball> Scene::balls = {
-    Ball(Vector2D(0.0, 0.0),Point2D(1200 * 1 / 4,70),Constants::BallRadius),
+    Ball(White,Filled,Vector2D(0.0, 0.0),Point2D(1200 * 1 / 4,700 / 2),Constants::BallRadius),
 
-	Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX,Constants::FirstBallY),Constants::BallRadius),
+	Ball(Yellow,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX,Constants::FirstBallY),Constants::BallRadius),
 
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * Constants::BallRadius,Constants::FirstBallY - Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * Constants::BallRadius,Constants::FirstBallY + Constants::BallRadius),Constants::BallRadius),
+    Ball(Red,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * Constants::BallRadius,Constants::FirstBallY - Constants::BallRadius),Constants::BallRadius),
+    Ball(Red,Striped,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * Constants::BallRadius,Constants::FirstBallY + Constants::BallRadius),Constants::BallRadius),
 
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * 2 * Constants::BallRadius,Constants::FirstBallY - 2 * Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * 2 * Constants::BallRadius,Constants::FirstBallY),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * 2 * Constants::BallRadius,Constants::FirstBallY + 2 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Green,Striped,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * 2 * Constants::BallRadius,Constants::FirstBallY - 2 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Black,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * 2 * Constants::BallRadius,Constants::FirstBallY),Constants::BallRadius),
+    Ball(Green,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 2 * 2 * Constants::BallRadius,Constants::FirstBallY + 2 * Constants::BallRadius),Constants::BallRadius),
 
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY - 3 * Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY - Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY + Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY + 3 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Yellow,Striped,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY - 3 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Purple,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY - Constants::BallRadius),Constants::BallRadius),
+    Ball(Brown,Striped,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY + Constants::BallRadius),Constants::BallRadius),
+    Ball(Orange,Striped,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 3 * 2 * Constants::BallRadius,Constants::FirstBallY + 3 * Constants::BallRadius),Constants::BallRadius),
 
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY - 4 * Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY - 2 * Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY + 2 * Constants::BallRadius),Constants::BallRadius),
-    Ball(Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY + 4 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Purple,Striped,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY - 4 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Orange,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY - 2 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Blue,Striped,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY),Constants::BallRadius),
+    Ball(Blue,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY + 2 * Constants::BallRadius),Constants::BallRadius),
+    Ball(Brown,Filled,Vector2D(-0.0,0.0),Point2D(Constants::FirstBallX + 4 * 2 * Constants::BallRadius,Constants::FirstBallY + 4 * Constants::BallRadius),Constants::BallRadius),
 };
 
 Cue Scene::cue = Cue(Point2D(300 - Constants::BallRadius,Constants::FirstBallY), Vector2D(-1.0,0.0), 250);
@@ -86,20 +87,60 @@ bool Scene::ballsMoving = false;
 
 double Scene::LastFrameDuration = 0;
 
-void drawBall(double x , double y , double radius) {
+void drawBall(BallColor color , BallType type, double x , double y , double radius) {
 
 	const double DEG2RAD = 3.14159/180;
 
-	glColor3d(0.0,0.0,1.0);
-	glPolygonMode(GL_FRONT,GL_FILL);
-	glBegin(GL_POLYGON);
-
-	for (double i = 0; i < 360; i+= 1)
-	{
-		float degInRad = i*DEG2RAD;
-		glVertex2f(x + cos(degInRad)*radius,y + sin(degInRad)*radius);
+	switch (color) {
+	    case Red : glColor3d(1,0,0);break;
+	    case Blue : glColor3d(0,0,1);break;
+	    case Yellow : glColor3d(1,1,0);break;
+	    case Purple: glColor3d(154.0 / 255.0,24.0 / 255.0, 201.0 / 255.0);break;
+	    case Green: glColor3d(10.0 / 255.0,148.0 / 255.0,22.0 / 255.0);break;
+	    case Black: glColor3d(0,0,0); break;
+	    case White: glColor3d(1,1,1);break;
+	    case Brown: glColor3d(0.61960784313,0.21960784313,0.01960784313);break;
+	    case Orange: glColor3d(0.96078431372,0.65882352941,0.00784313725);break;
 	}
-	glEnd();
+
+	switch (type) {
+	    case Filled: {
+	        glPolygonMode(GL_FRONT,GL_FILL);
+            glBegin(GL_POLYGON);
+
+            for (double i = 0; i < 360; i+= 1)
+            {
+                float degInRad = i*DEG2RAD;
+                glVertex2f(x + cos(degInRad)*radius,y + sin(degInRad)*radius);
+            }
+            glEnd();
+            break;
+	    }
+	    /* no break */
+	    case Striped: {
+	        glPolygonMode(GL_FRONT,GL_FILL);
+            glBegin(GL_POLYGON);
+
+            for (double i = 0; i < 360; i+= 1)
+            {
+                float degInRad = i*DEG2RAD;
+                glVertex2f(x + cos(degInRad)*radius,y + sin(degInRad)*radius);
+            }
+            glEnd();
+	        radius -= radius / 4 * 3;
+	        glColor3d(1,1,1);
+	        glPolygonMode(GL_FRONT,GL_FILL);
+            glBegin(GL_POLYGON);
+
+            for (double i = 0; i < 360; i+= 1)
+            {
+                float degInRad = i*DEG2RAD;
+                glVertex2f(x + cos(degInRad)*radius,y + sin(degInRad)*radius);
+            }
+            glEnd();
+	    }
+	}
+
 }
 
 void drawCircle(double x , double y , double radius) {
@@ -140,32 +181,31 @@ void Scene::Movement(void) {
 
     for (unsigned int ballIndex1 = 0; ballIndex1 < balls.size(); ballIndex1 ++ ) {
 
-            Ball * const ball1 = &balls[ballIndex1];
-            Vector2D currentBallVelocity = ball1->Direction;
-            ball1->Direction *= frameRatio;
+        Ball * const ball1 = &balls[ballIndex1];
+        Vector2D currentBallVelocity = ball1->Direction;
+        ball1->Direction *= frameRatio;
 
-            double minCollisionTime = numeric_limits<double>::max();
+        double minCollisionTime = numeric_limits<double>::max();
 
-            for (unsigned int tableMarginIndex = 0; tableMarginIndex < tableMargins.size(); tableMarginIndex ++) {
-                double collisionTime = ball1->PredictCollisionTime(tableMargins[tableMarginIndex]);
-                if (collisionTime >= 0 && collisionTime < 1 && collisionTime < minCollisionTime) {
-                    minCollisionTime = collisionTime;
-                }
+        for (unsigned int tableMarginIndex = 0; tableMarginIndex < tableMargins.size(); tableMarginIndex ++) {
+            double collisionTime = ball1->PredictCollisionTime(tableMargins[tableMarginIndex]);
+            if (collisionTime >= 0 && collisionTime < 1 && collisionTime < minCollisionTime) {
+                minCollisionTime = collisionTime;
             }
+        }
 
-            for (unsigned int ballIndex2 = ballIndex1 + 1 ; ballIndex2 < balls.size() ; ballIndex2 ++ ) {
-                double collisionTime = ball1->PredictCollisionTime(balls[ballIndex2]);
-                if (collisionTime >= 0 && collisionTime < 1 && collisionTime < minCollisionTime) {
-                    minCollisionTime = collisionTime;
-                }
+        for (unsigned int ballIndex2 = ballIndex1 + 1 ; ballIndex2 < balls.size() ; ballIndex2 ++ ) {
+            double collisionTime = ball1->PredictCollisionTime(balls[ballIndex2]);
+            if (collisionTime >= 0 && collisionTime < 1 && collisionTime < minCollisionTime) {
+                minCollisionTime = collisionTime;
             }
+        }
 
-            ball1->Direction = currentBallVelocity;
-            if (minCollisionTime != numeric_limits<double>::max()) {
-                ball1->Center.X += ball1->Direction.X * frameRatio * minCollisionTime;
-                ball1->Center.Y += ball1->Direction.Y * frameRatio * minCollisionTime;
-            }
-
+        ball1->Direction = currentBallVelocity;
+        if (minCollisionTime != numeric_limits<double>::max()) {
+            ball1->Center.X += ball1->Direction.X * frameRatio * minCollisionTime;
+            ball1->Center.Y += ball1->Direction.Y * frameRatio * minCollisionTime;
+        }
     }
 
     for (unsigned int ballIndex1 = 0; ballIndex1 < balls.size(); ballIndex1 ++ ) {
@@ -231,9 +271,19 @@ void Scene::Movement(void) {
 void Scene::Render(void) {
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(138.0 / 255.0, 96.0 / 255.0, 18.0 / 255.0, 1.0);
+
+    glPolygonMode(GL_FRONT,GL_FILL);
+    glColor3d(0,1,0);
+    glBegin(GL_POLYGON);
+    for (unsigned int circleIndex = 0; circleIndex < tablePockets.size(); circleIndex++) {
+        glVertex2d(tablePockets[circleIndex].Center.X, tablePockets[circleIndex].Center.Y);
+    }
+    glEnd();
+
 
     for (const Segment & margin : tableMargins) {
-        glColor3d(0.0, 0.0, 1.0);
+        glColor3d(0.0, 0.0, 0.0);
         glLineWidth(3.0);
         glBegin(GL_LINES);
         glVertex2d(margin.LeftPoint.X, margin.LeftPoint.Y);
@@ -242,11 +292,11 @@ void Scene::Render(void) {
     }
 
     for (const Circle & circle : tablePockets) {
-        drawCircle(circle.Center.X, circle.Center.Y, circle.Radius);
+        drawBall(Black,Filled,circle.Center.X, circle.Center.Y, circle.Radius);
     }
 
     for (const Ball & ball : balls) {
-        drawBall(ball.Center.X, ball.Center.Y, ball.Radius);
+        drawBall(ball.Color,ball.Type,ball.Center.X, ball.Center.Y, ball.Radius);
     }
 
     if (!ballsMoving) {
